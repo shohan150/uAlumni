60cdab9c7762866036e24e6b5b5b14cd48f8172d
shohan <shohan.ece17@gmail.com>
2024-11-12 17:36:37 +0600

    done storing access

--- src/providers/AuthProvider.jsx
+++ src/providers/AuthProvider.jsx
@@ -1,7 +1,15 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { authContext } from "../context";
 const AuthProvider = ({ children }) => {
-  const [auth, setAuth] = useState({});
+  const [auth, setAuth] = useState(() => {
+    const storedAuth = localStorage.getItem('ualumni-authInfo');
+    return storedAuth ? JSON.parse(storedAuth) : {};
+  });
+
+  useEffect(() => {
+    localStorage.setItem('ualumni-authInfo', JSON.stringify(auth));
+  }, [auth]);
+
   return (
     <authContext.Provider value={{ auth, setAuth }}>
       {children}
